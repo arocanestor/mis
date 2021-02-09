05d883d0c5d26f3d111ce98a21731f1f6bb181f3
pelanguero <pelanguero@gmail.com>
2021-02-08 22:05:30 -0500

    endpoint

--- front/src/components/Calendar.js
+++ front/src/components/Calendar.js
@@ -12,7 +12,7 @@ import {
   FormGroup,
   Label,
 } from "reactstrap";
-var endpoint = "http://apicalendariomisas.argodevs.com";
+var endpoint = "http://apicalendariomisas.argodevs.com/";
 export default class DemoApp extends Component {
   constructor(props) {
     super(props);
